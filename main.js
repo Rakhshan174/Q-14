e8bf4763500169f998749b83c08a3f47aad00821
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 06:10:04 -0700

    Q 14 TS/JS for 45 assignment

---
+++ main.js
@@ -0,0 +1,5 @@
+//Guest List:If you could invite anyone, living or deceased, to dinner, who would you invite? 
+//Make a list that at least three people you'd like to invite to dinner.Then use your list to
+//print a meesage to each person. inviting them to dinner.
+var guestArr = ["Ali", "Haseeb", "Wajahat"];
+guestArr.map(function (items) { return (console.log("Dear ".concat(items, ", your are invited to dinnder"))); });
---
+++ main.ts
@@ -0,0 +1,7 @@
+//Guest List:If you could invite anyone, living or deceased, to dinner, who would you invite? 
+//Make a list that at least three people you'd like to invite to dinner.Then use your list to
+//print a meesage to each person. inviting them to dinner.
+
+let guestArr : string [] = ["Ali", "Haseeb", "Wajahat"];
+
+guestArr.map((items) => (console.log(`Dear ${items}, your are invited to dinnder`)))
